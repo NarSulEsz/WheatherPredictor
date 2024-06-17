e5d83f79874a9f63d72ed44eb6c5b8f17f71c10a
Nariman Suleimenov <162845160+NarSulEsz@users.noreply.github.com>
2024-06-16 23:34:07 -0400

    added REDME

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# WheatherPredictor
+# Module06-WheatherPredictor
 Using open API to create mobile app for weather forecast
+## Description
+
+This project presents the application to forecast the weather
+
+## Table of Contents (Optional)
+
+Not provided
+    
+
+## Installation
+
+No special skills needed
+
+## Usage
+
+No special instructions needed
+This is link of repository: https://github.com/NarSulEsz/WheatherPredictor
+
+This is link of web-site itself: https://narsulesz.github.io/task-list/
+
+Screenshot without dialog widget: https://github.com/NarSulEsz/task-list/blob/main/assets/scrinshots/Screenshot%202024-06-10%20202209.png
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,14 @@
-const usersContainer = document.getElementById('users');
+const citiesContainer = document.getElementById('cities');
+const city = document.getElementById('city-of-user');
 const fetchButton = document.getElementById('fetch-button');
-let city = document.getElementById('city-of-user');
 const APIKey = "15a686f297cf37379980ae5889d59df9";
 
 
-async function getApi() {
-  const coordUrl = `${weatherApiRootUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherApiKey}`;
-  
-  const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${APIKey}`;
-  console.log("testing");
-console.log(queryURL)
-  try {
-    const response = await fetch(queryURL);
-    const movies = await response.json();
-    console.log('response', movies)
-  } catch(err){
-    console.log(err);
-  }
+function getApi(event) {
+  event.preventDefault();
 
- /* fetch(queryURL)
+  const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${APIKey}`;
+    fetch(queryURL)
     .then(function (response) {
       return response.json();
     })
@@ -26,22 +16,21 @@ console.log(queryURL)
       //Using console.log to examine the data
       console.log(data);
       for (let i = 0; i < data.length; i++) {
-        //Creating a h3 element and a p element
-        const cities = document.createElement('h3');
-        const userUrl = document.createElement('p');
+        //Creating a "ul" element and a "li" element
+        const listOfCities = document.createElement('ul');
+        const currentWeather = document.createElement('li');
 
         //Setting the text of the h3 element and p element.
-        userName.textContent = data[i].login;
-        userUrl.textContent = data[i].html_url;
+        listOfCities.textContent = data[i].login;
+        currentWeather.textContent = data[i].html_url;
 
         //Appending the dynamically generated html to the div associated with the id="users"
         //Append will attach the element as the bottom most child.
-        usersContainer.append(userName);
-        usersContainer.append(userUrl);
+        citiesContainer.appendChild(listOfCities);
+        listOfCities.appendChild(currentWeather);
+        return citiesContainer;
       }
     });
-*/
+
 }
-fetchButton.addEventListener('click', async function(){
-  await getApi();
-});
+fetchButton.addEventListener('click', getApi);
